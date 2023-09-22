90bacb5e4fe8ffaa0302eca767bee8504a12ce53
Mary KIm <jkim1471@gmail.com>
2023-09-22 15:19:24 -0500

    Added New Activity and onClick Functions

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.java
@@ -2,10 +2,26 @@ package com.cs407.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickAdd(View view){
+        // check if valid input (int) is entered
+        EditText input1 = (EditText) findViewById(R.id.input1); // get text from input1
+        EditText input2 = (EditText) findViewById(R.id.input2); // get text from input1
+
+
+    }
+    public void goToActivity(String s){
+        Intent intent = new Intent(this, ShowResult.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
