5ebf6695917d4e254807a562ab83cdd10aa37393
Mary KIm <jkim1471@gmail.com>
2023-09-25 10:51:07 -0500

    Update

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.java
@@ -10,12 +10,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickAdd(View view){
+    public void buttonClicked(View view){
         // check if valid input (int) is entered
         EditText input1 = (EditText) findViewById(R.id.input1); // get text from input1
         EditText input2 = (EditText) findViewById(R.id.input2); // get text from input1
+        int num1 = Integer.parseInt(input1.getText().toString());
+        int num2 = Integer.parseInt(input2.getText().toString());
 
-
+        if(view.getId() == R.id.addButton){
+            goToActivity(Integer.toString(num1+num2));
+        } else if(view.getId() == R.id.subtractButton) {
+            goToActivity(Integer.toString(num1-num2));
+        } else if(view.getId() == R.id.timesButton) {
+            goToActivity(Integer.toString(num1*num2));
+        } else if(view.getId() == R.id.divideButton) {
+            if(num2 == 0)
+                Toast.makeText(MainActivity.this, "Please enter non-zero values.", Toast.LENGTH_LONG).show();
+            else
+                goToActivity(Integer.toString(num1/num2));
+        } else {
+            Toast.makeText(MainActivity.this, "Unrecognized button clicked.", Toast.LENGTH_LONG).show();
+        }
     }
     public void goToActivity(String s){
         Intent intent = new Intent(this, ShowResult.class);
--- app/src/main/java/com/cs407/calculatorapp/ShowResult.java
+++ app/src/main/java/com/cs407/calculatorapp/ShowResult.java
@@ -2,13 +2,20 @@ package com.cs407.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowResult extends AppCompatActivity {
 
+    TextView resultView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
+        resultView = (TextView) findViewById(R.id.resultView);
+        Intent intent=getIntent();
+        String str = intent.getStringExtra("message");
+        resultView.setText(str);
     }
 }
