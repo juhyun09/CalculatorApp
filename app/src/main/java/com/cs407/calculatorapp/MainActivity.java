58ee687bdb78765844f71d6f4ec266948e6a61e1
Mary KIm <jkim1471@gmail.com>
2023-09-25 12:23:16 -0500

    Update (Working Calculator)

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,23 +12,29 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked(View view){
+        Log.i("INFO", "Button Pressed");
         // check if valid input (int) is entered
         EditText input1 = (EditText) findViewById(R.id.input1); // get text from input1
         EditText input2 = (EditText) findViewById(R.id.input2); // get text from input1
         int num1 = Integer.parseInt(input1.getText().toString());
         int num2 = Integer.parseInt(input2.getText().toString());
+        Log.i("Info", "num1 = " + num1 + " num2 = "+ num2);
 
         if(view.getId() == R.id.addButton){
+            Log.i("Info", "add clicked");
             goToActivity(Integer.toString(num1+num2));
         } else if(view.getId() == R.id.subtractButton) {
+            Log.i("Info", "subtract clicked");
             goToActivity(Integer.toString(num1-num2));
         } else if(view.getId() == R.id.timesButton) {
+            Log.i("Info", "times clicked");
             goToActivity(Integer.toString(num1*num2));
         } else if(view.getId() == R.id.divideButton) {
+            Log.i("Info", "divide clicked");
             if(num2 == 0)
-                Toast.makeText(MainActivity.this, "Please enter non-zero values.", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Please enter non-zero value for divisor.", Toast.LENGTH_LONG).show();
             else
-                goToActivity(Integer.toString(num1/num2));
+                goToActivity(Double.toString(((double) num1)/((double)num2)));
         } else {
             Toast.makeText(MainActivity.this, "Unrecognized button clicked.", Toast.LENGTH_LONG).show();
         }
